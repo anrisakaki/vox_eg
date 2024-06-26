c7f07e04d4d60daa8617a5b580a26d42f98cffa7
anrisakaki <86014655+anrisakaki@users.noreply.github.com>
2024-06-26 14:57:28 +0100

    Add files via upload

--- Test.R
+++ Test.R
@@ -7,6 +7,8 @@ set.seed(123)
 
 # Parameters
 N <- 1000  # Total number of calls
+min_calls_per_employee <- 10  # Minimum number of calls per employee
+max_calls_per_employee <- 185  # Maximum number of calls per employee
 num_counterparties <- 10  # Minimum number of counterparties each employee speaks to
 max_counterparties <- 15  # Maximum number of counterparties each employee speaks to
 predominant_counterparties <- 3  # Number of counterparties that are predominant
@@ -22,17 +24,17 @@ counterparties <- c("Goldman Sachs", "JP Morgan", "Morgan Stanley", "Citigroup",
                     "Societe Generale", "Unicredit", "BNY Mellon", "State Street", "TP ICAP",
                     "ICAP", "BGC Partners", "CME Group", "Tullett Prebon", "Virtu Financial")
 
-# Generate employee data
 employee_data <- data.frame(
   employee_id = seq_along(employee_names),
   employee_name = employee_names
 )
 
 generate_counterparties <- function() {
+  num_calls <- sample(min_calls_per_employee:max_calls_per_employee, 1)
   num_counterparties <- sample(num_counterparties:max_counterparties, 1)
   predominant <- sample(counterparties, predominant_counterparties)
   other <- sample(setdiff(counterparties, predominant), num_counterparties - predominant_counterparties)
-  c(predominant, sample(other, num_counterparties, replace = TRUE))  # Shuffle to mix predominant and other counterparties
+  c(predominant, sample(other, num_counterparties, replace = TRUE))  
 }
 
 counterparty_data <- replicate(N, generate_counterparties())
@@ -46,20 +48,35 @@ counterparty_data <- sample(counterparty_data)
 hsbc_indices <- sample(1:N, N * 0.3)
 counterparty_data[hsbc_indices] <- "HSBC"
 
-start_date <- as.Date("2023-01-01")
-end_date <- as.Date("2023-12-31")
+start_date <- as.Date("2024-06-17")
+end_date <- as.Date("2024-06-21")
 dates <- sample(seq.Date(start_date, end_date, by = "day"), N, replace = TRUE)
 uk_dates <- format(dates, "%d/%m/%Y")
 
+calls_per_employee <- sample(min_calls_per_employee:max_calls_per_employee, length(employee_names), replace = TRUE)
+
 df1 <- data.frame(
-  call_id = 1:N,
-  date = uk_dates,
-  employee_name = rep(employee_data$employee_name, each = ceiling(N / nrow(employee_data))),
-  employee_id = rep(employee_data$employee_id, each = ceiling(N / nrow(employee_data))),
-  counterparty = counterparty_data
+  call_id = integer(N),
+  date = character(N),
+  employee_name = character(N),
+  employee_id = integer(N),
+  counterparty = character(N),
+  stringsAsFactors = FALSE
 )
 
-df1 <- df1[1:N, ]
+row_idx <- 1
+for (i in seq_along(employee_names)) {
+  for (j in seq_len(calls_per_employee[i])) {
+    if (row_idx > N) break  
+    
+    df1$call_id[row_idx] <- row_idx
+    df1$date[row_idx] <- uk_dates[row_idx]
+    df1$employee_name[row_idx] <- employee_names[i]
+    df1$employee_id[row_idx] <- employee_data$employee_id[i]
+    df1$counterparty[row_idx] <- counterparty_data[row_idx]
+    row_idx <- row_idx + 1
+  }
+}
 
 # Save dataset to CSV
 write.csv(df1, "vox_eg.csv", row.names = FALSE)
@@ -69,6 +86,10 @@ write.csv(df1, "vox_eg.csv", row.names = FALSE)
 ####################
 
 emp_sum <- df1 %>% 
+  group_by(employee_name) %>% 
+  summarise(n = n())
+
+empcounterparty_sum <- df1 %>% 
   group_by(employee_name, counterparty) %>% 
   summarise(n = n())
 
